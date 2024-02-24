05f2c47fa03c96ac9da34885022c016de93b6524
andrenormanlang <andrenormanlang@gmail.com>
2024-02-24 16:47:45 +0100

    header & user input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+
 function App() {
-    return (
-      <h1>React Investment Calculator</h1>
-    )
-  }
-  
-  export default App
+	return (
+		<>
+			<Header />;
+			<UserInput />
+		</>
+	);
+}
+
+export default App;
